26f841aee1c069fc08913694b0f1da027027f8f2
Strahinja <you@example.com>
2024-06-04 10:40:30 +0200

    Initial Commit

---
+++ Kotrola_toka.py
@@ -0,0 +1,20 @@
+speed = 11
+if(speed < 10):
+    print("To slow...")
+
+else:
+    print("Not to slow")
+
+
+for x in range(0, 5):
+    print("Hello World")
+
+
+i = 0
+while(i < 5):
+    print("Hello World")
+    i = i + 1
+
+
+
+
